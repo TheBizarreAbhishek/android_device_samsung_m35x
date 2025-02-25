7131b4c17bffb3ea264b025d7da4b7a5b523c9cf
Abhishek Babu <164913157+TheBizarreAbhishek@users.noreply.github.com>
2025-02-25 17:50:53 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_m35x-user
-add_lunch_combo omni_m35x-userdebug
-add_lunch_combo omni_m35x-eng
+add_lunch_combo twrp_m35x-user
+add_lunch_combo twrp_m35x-userdebug
+add_lunch_combo twrp_m35x-eng
