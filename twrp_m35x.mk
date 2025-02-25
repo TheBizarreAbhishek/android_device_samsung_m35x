a4a26c23e328ce8ee6bcfd8cce52efa7aedac4c5
Abhishek Babu <164913157+TheBizarreAbhishek@users.noreply.github.com>
2025-02-25 17:53:05 +0530

    Update and rename omni_m35x.mk to twrp_m35x.mk

--- twrp_m35x.mk
+++ twrp_m35x.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common Twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from m35x device
 $(call inherit-product, device/samsung/m35x/device.mk)
 
 PRODUCT_DEVICE := m35x
-PRODUCT_NAME := omni_m35x
+PRODUCT_NAME := twrp_m35x
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-M356B
 PRODUCT_MANUFACTURER := samsung
