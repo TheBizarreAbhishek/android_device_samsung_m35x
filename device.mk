5879e56050096e257d9a29e7622a22709e3c45cc
mrfluffyoven <mrfluffyoven@gmail.com>
2025-12-19 18:21:30 +0000

    fix missing seperator bug
    Change-Id: If190f1c7282716626f2acd7f05c2f862ace4f237

--- device.mk
+++ device.mk
@@ -13,16 +13,17 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
-    fastbootd \
+    fastbootd
 
 # Health
 PRODUCT_PACKAGES += \ 
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service \
+    android.hardware.health@2.1-service
 
+# sideload
 PRODUCT_PACKAGES += \
     otapreopt_script \
     update_engine \
     update_engine_sideload \
-    update_verifier \
+    update_verifier
