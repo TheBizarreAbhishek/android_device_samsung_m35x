305ed50c1d3764ddf805d63d4456f97ba3a6b9b7
mrfluffyoven <mrfluffyoven@gmail.com>
2025-12-19 08:30:39 +0000

    fix health in device.mk
    Change-Id: I4100ce1c07f50178c47364fad2928ab7e811af60

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \ 
-    android.hardware.health@2.1-impl
+    android.hardware.health@2.1-impl \
     android.hardware.health@2.1-impl.recovery \ 
     android.hardware.health@2.1-service
 
